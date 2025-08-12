c7040b0d75a975406b0e18faa87014a23c9f93ae
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-08-12 13:41:30 +0300

    options: change some general defaults for neovim
    This is more helpful to align to more used codding styles.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -38,7 +38,7 @@ vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
 vim.g.c_syntax_for_h = 1 -- Detect .h header files as C files
--- vim.o.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
--- vim.o.tabstop = 4 -- Insert n spaces for a tab (default: 8)
--- vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
--- vim.o.expandtab = true -- Convert tabs to spaces (default: false)
+vim.o.shiftwidth = 4 -- The number of spaces inserted for each indentation (default: 8)
+vim.o.tabstop = 4 -- Insert n spaces for a tab (default: 8)
+vim.o.softtabstop = 4 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
+vim.o.expandtab = true -- Convert tabs to spaces (default: false)
