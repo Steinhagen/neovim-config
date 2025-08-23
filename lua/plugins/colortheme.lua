6afb373b8b093f73f295d25bd540b107aacba753
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-08-23 22:39:41 +0300

    colortheme: added option to have multiple colorthemes selectable

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,140 +1,117 @@
 return {
-  'catppuccin/nvim',
-  as = 'catppuccin',
-  lazy = false, -- make sure we load this during startup
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    local utils = require 'utils.theme'
+  (function()
+    -- Define all your themes in here
+    local themes = {
+      catppuccin = {
+        plugin = 'catppuccin/nvim',
+        -- The name used for the :colorscheme command
+        colorscheme = 'catppuccin',
+        -- Function that returns the setup options table
+        opts = function(transparent)
+          return {
+            flavour = 'auto',
+            background = { light = 'latte', dark = 'mocha' },
+            transparent_background = transparent,
+            styles = {
+              comments = { 'italic' },
+              conditionals = { 'italic' },
+            },
+            integrations = {
+              cmp = true,
+              gitsigns = true,
+              nvimtree = true,
+              treesitter = true,
+            },
+          }
+        end,
+      },
+      tokyonight = {
+        plugin = 'folke/tokyonight.nvim',
+        colorscheme = 'tokyonight',
+        opts = function(transparent)
+          return {
+            style = 'night',
+            light_style = 'day',
+            transparent = transparent,
+            terminal_colors = true,
+            styles = {
+              keywords = { italic = true },
+              sidebars = transparent and 'transparent' or 'dark',
+              floats = transparent and 'transparent' or 'dark',
+            },
+          }
+        end,
+      },
+    }
 
-    -- Initial states
-    local bg_transparent = true
+    -- This is the name of the active theme
+    local current_theme_name = 'catppuccin'
 
-    -- Function to setup tokyonight with current settings
-    -- local setup_tokyonight = function(transparent)
-    --   require('tokyonight').setup {
-    --     -- your configuration comes here
-    --     -- or leave it empty to use the default settings
-    --     style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-    --     light_style = 'day', -- The theme is used when the background is set to light
-    --     transparent = transparent, -- Enable this to disable setting the background color
-    --     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-    --     styles = {
-    --       -- Style to be applied to different syntax groups
-    --       keywords = { italic = true },
-    --       functions = {},
-    --       variables = {},
-    --       -- Background styles. Can be "dark", "transparent" or "normal"
-    --       sidebars = 'transparent', -- style for sidebars, see below
-    --       floats = 'transparent', -- style for floating windows
-    --     },
-    --     sidebars = { 'qf', 'help', 'neo-tree', 'terminal', 'packer' }, -- Set a darker background on sidebar-like windows
-    --     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style
-    --     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines
-    --     dim_inactive = false, -- dims inactive windows
-    --     lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
-    --   }
-    -- end
-
-    -- Function to setup catppuccin with current settings
-    local setup_catppuccin = function(transparent)
-      require('catppuccin').setup {
-        flavour = 'auto', -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-          light = 'latte',
-          dark = 'mocha',
-        },
-        transparent_background = transparent, -- disables setting the background color.
-        float = {
-          transparent = false, -- enable transparent floating windows
-          solid = false, -- use solid styling for floating windows, see |winborder|
-        },
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
-          shade = 'dark',
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false, -- Force no italic
-        no_bold = false, -- Force no bold
-        no_underline = false, -- Force no underline
-        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { 'italic' }, -- Change the style of comments
-          conditionals = { 'italic' },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-          -- miscs = {}, -- Uncomment to turn off hard-coded styles
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        default_integrations = true,
-        auto_integrations = false,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = '',
-          },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-      }
+    local active_theme = themes[current_theme_name]
+    if not active_theme then
+      vim.notify('Theme "' .. current_theme_name .. '" not found in configuration.', vim.log.levels.ERROR)
+      return {} -- Return empty spec to avoid errors
     end
 
-    -- Auto-detect system theme on startup
-    local is_dark_theme = utils.detect_system_theme()
+    -- Return the plugin spec for lazy.nvim
+    return {
+      active_theme.plugin,
+      as = active_theme.colorscheme, -- Use 'as' in case the plugin name is different
+      lazy = false,
+      priority = 1000,
+      config = function()
+        local utils = require 'utils.theme'
 
-    -- Toggle background transparency
-    local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      -- Reconfigure tokyonight with current settings
-      setup_catppuccin(bg_transparent)
-      -- Reload the colorscheme to apply changes
-      vim.cmd [[colorscheme catppuccin]]
-    end
+        -- Initial states
+        local bg_transparent = false -- Set your default transparency
+        local is_dark_theme = utils.detect_system_theme()
 
-    -- Toggle between dark and light themes (manual override)
-    local toggle_theme = function()
-      is_dark_theme = not is_dark_theme
-      -- Set vim background to match the theme
-      vim.o.background = is_dark_theme and 'dark' or 'light'
-      -- Reconfigure tokyonight with current settings
-      setup_catppuccin(bg_transparent)
-      -- Reload the colorscheme to apply changes
-      vim.cmd [[colorscheme catppuccin]]
-    end
+        -- Generic function to setup and apply the current theme
+        local apply_theme = function()
+          -- 1. Get the correct options based on transparency
+          local theme_opts = active_theme.opts(bg_transparent)
+          -- 2. Call the theme's setup function
+          require(active_theme.colorscheme).setup(theme_opts)
+          -- 3. Set the colorscheme
+          vim.cmd.colorscheme(active_theme.colorscheme)
+        end
 
-    -- Function to refresh theme based on current system settings
-    local refresh_system_theme = function()
-      local current_system_theme = utils.detect_system_theme()
-      if current_system_theme ~= is_dark_theme then
-        is_dark_theme = current_system_theme
+        -- Toggle background transparency
+        local toggle_transparency = function()
+          bg_transparent = not bg_transparent
+          apply_theme()
+          vim.notify('Transparency ' .. (bg_transparent and 'enabled' or 'disabled'))
+        end
+
+        -- Toggle between dark and light themes
+        local toggle_theme = function()
+          is_dark_theme = not is_dark_theme
+          vim.o.background = is_dark_theme and 'dark' or 'light'
+          apply_theme()
+          vim.notify('Theme set to ' .. (is_dark_theme and 'dark' or 'light'))
+        end
+
+        -- Function to refresh theme based on system settings
+        local refresh_system_theme = function()
+          local current_system_theme = utils.detect_system_theme()
+          if current_system_theme ~= is_dark_theme then
+            is_dark_theme = current_system_theme
+            vim.o.background = is_dark_theme and 'dark' or 'light'
+            apply_theme()
+            vim.notify('System theme changed to ' .. (is_dark_theme and 'dark' or 'light'))
+          end
+        end
+
+        -- Initial setup
         vim.o.background = is_dark_theme and 'dark' or 'light'
-        setup_catppuccin(bg_transparent)
-        vim.cmd [[colorscheme catppuccin]]
-      end
-    end
+        apply_theme()
 
-    -- Initial setup with detected theme
-    setup_catppuccin(bg_transparent)
-    vim.o.background = is_dark_theme and 'dark' or 'light'
-    vim.cmd [[colorscheme catppuccin]]
-
-    -- Set up the keymaps
-    local opts = { noremap = true, silent = true }
-    vim.keymap.set('n', '<leader>bgt', toggle_transparency, vim.tbl_extend('force', opts, { desc = 'Toggle theme background transparency' }))
-    vim.keymap.set('n', '<leader>bgs', toggle_theme, vim.tbl_extend('force', opts, { desc = "Toggle between the theme's Light/Dark modes" }))
-    vim.keymap.set('n', '<leader>bgr', refresh_system_theme, vim.tbl_extend('force', opts, { desc = 'Get the theme mode based on the host OS' }))
-  end,
+        -- Set up the keymaps
+        local opts = { noremap = true, silent = true }
+        vim.keymap.set('n', '<leader>bgt', toggle_transparency, vim.tbl_extend('force', opts, { desc = 'Toggle BG transparency' }))
+        vim.keymap.set('n', '<leader>bgs', toggle_theme, vim.tbl_extend('force', opts, { desc = 'Toggle Light/Dark mode' }))
+        vim.keymap.set('n', '<leader>bgr', refresh_system_theme, vim.tbl_extend('force', opts, { desc = 'Refresh system theme' }))
+      end,
+    }
+  end)(),
 }
