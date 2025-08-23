09db8b40d4786180587a0ba170b0c49b0e554281
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-08-23 12:51:53 +0300

    neovim-tips: update configuration
    Update the configuration to warn on duplicate tips and also have a
    shortcut to display the help manual.

--- lua/plugins/neovim-tips.lua
+++ lua/plugins/neovim-tips.lua
@@ -4,11 +4,16 @@ return {
   opts = {
     -- OPTIONAL: Location of user defined tips (default value shown below)
     user_file = vim.fn.stdpath 'config' .. '/lua/plugins/data/neovim-tips/user-tips.txt',
+    -- OPTIONAL: Show warnings when user tips conflict with builtin (default: true)
+    user_tip_prefix = '[User] ',
+    -- OPTIONAL: Show warnings when user tips conflict with builtin (default: true)
+    warn_on_conflicts = true,
   },
   init = function()
     local map = vim.keymap.set
     map('n', '<leader>nto', ':NeovimTips<CR>', { desc = 'Neovim tips', noremap = true, silent = true })
     map('n', '<leader>nte', ':NeovimTipsEdit<CR>', { desc = 'Edit your Neovim tips', noremap = true, silent = true })
     map('n', '<leader>nta', ':NeovimTipsAdd<CR>', { desc = 'Add your Neovim tip', noremap = true, silent = true })
+    map('n', '<leader>nth', ':NeovimTipsHelp<CR>', { desc = 'Neovim tips user guide', noremap = true, silent = true })
   end,
 }
