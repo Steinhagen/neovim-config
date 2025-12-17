0a2a211158b191b1d09edacd6ef76f36f5eed829
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-12-17 11:14:16 +0200

    codecompanion: update to latest version
    Update the latest code to the latest version and switch the default model to Gemini.

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,8 +4,8 @@ return {
   'olimorris/codecompanion.nvim',
   opts = {
     strategies = {
-      chat = { adapter = 'ollama_chat' }, -- gemini
-      inline = { adapter = 'ollama_inline' }, -- gemini,
+      chat = { adapter = 'gemini' },
+      inline = { adapter = 'gemini' },
     },
 
     adapters = {
@@ -19,6 +19,7 @@ return {
             },
           })
         end,
+
         ollama_chat = function()
           return require('codecompanion.adapters').extend('ollama', {
             schema = {
@@ -28,20 +29,24 @@ return {
             },
           })
         end,
+
         gemini = function()
           return require('codecompanion.adapters').extend('gemini', {
-            -- url = 'https://generativelanguage.googleapis.com/v1beta/models/${model}:streamGenerateContent?alt=sse&key=${api_key}',
             env = {
               api_key = os.getenv 'GEMINI_API_KEY',
-              -- model = 'schema.model.default',
-              -- model = 'gemini-2.5-flash',
-              default = 'gemini-2.5-pro',
+            },
+            schema = {
+              model = {
+                -- choose one of: gemini-2.0-flash, gemini-2.0-pro, 2.5, etc.
+                default = 'gemini-2.5-flash',
+              },
             },
           })
         end,
       },
     },
   },
+
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
@@ -50,7 +55,6 @@ return {
       config = function()
         local diff = require 'mini.diff'
         diff.setup {
-          -- Disabled by default
           source = diff.gen_source.none(),
         }
       end,
@@ -58,77 +62,17 @@ return {
   },
 
   keys = {
-    -- Recommend setup
-    {
-      mapping_key_prefix .. 'a',
-      '<cmd>CodeCompanionActions<cr>',
-      desc = 'Code Companion - Actions',
-    },
-    {
-      mapping_key_prefix .. 't',
-      '<cmd>CodeCompanionChat Toggle<cr>',
-      desc = 'Code Companion - Toggle',
-      mode = { 'n', 'v' },
-    },
-    -- Some common usages with visual mode
-    {
-      mapping_key_prefix .. 'e',
-      '<cmd>CodeCompanion /explain<cr>',
-      desc = 'Code Companion - Explain code',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'f',
-      '<cmd>CodeCompanion /fix<cr>',
-      desc = 'Code Companion - Fix code',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'l',
-      '<cmd>CodeCompanion /lsp<cr>',
-      desc = 'Code Companion - Explain LSP diagnostic',
-      mode = { 'n', 'v' },
-    },
-    {
-      mapping_key_prefix .. 'T',
-      '<cmd>CodeCompanion /tests<cr>',
-      desc = 'Code Companion - Generate unit test',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'm',
-      '<cmd>CodeCompanion /staged-commit<cr>',
-      desc = 'Code Companion - Git commit message (staged)',
-    },
-    {
-      mapping_key_prefix .. 'd',
-      '<cmd>CodeCompanion /inline-doc<cr>',
-      desc = 'Code Companion - Inline document code',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'D',
-      '<cmd>CodeCompanion /doc<cr>',
-      desc = 'Code Companion - Document code',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'r',
-      '<cmd>CodeCompanion /refactor<cr>',
-      desc = 'Code Companion - Refactor code',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'R',
-      '<cmd>CodeCompanion /review<cr>',
-      desc = 'Code Companion - Review code',
-      mode = 'v',
-    },
-    {
-      mapping_key_prefix .. 'n',
-      '<cmd>CodeCompanion /naming<cr>',
-      desc = 'Code Companion - Better naming',
-      mode = 'v',
-    },
+    { mapping_key_prefix .. 'a', '<cmd>CodeCompanionActions<cr>', desc = 'Code Companion - Actions' },
+    { mapping_key_prefix .. 't', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Code Companion - Toggle', mode = { 'n', 'v' } },
+    { mapping_key_prefix .. 'e', '<cmd>CodeCompanion /explain<cr>', desc = 'Code Companion - Explain code', mode = 'v' },
+    { mapping_key_prefix .. 'f', '<cmd>CodeCompanion /fix<cr>', desc = 'Code Companion - Fix code', mode = 'v' },
+    { mapping_key_prefix .. 'l', '<cmd>CodeCompanion /lsp<cr>', desc = 'Code Companion - Explain LSP', mode = { 'n', 'v' } },
+    { mapping_key_prefix .. 'T', '<cmd>CodeCompanion /tests<cr>', desc = 'Code Companion - Generate tests', mode = 'v' },
+    { mapping_key_prefix .. 'm', '<cmd>CodeCompanion /staged-commit<cr>', desc = 'Code Companion - Commit msg' },
+    { mapping_key_prefix .. 'd', '<cmd>CodeCompanion /inline-doc<cr>', desc = 'Code Companion - Inline docs', mode = 'v' },
+    { mapping_key_prefix .. 'D', '<cmd>CodeCompanion /doc<cr>', desc = 'Code Companion - Docs', mode = 'v' },
+    { mapping_key_prefix .. 'r', '<cmd>CodeCompanion /refactor<cr>', desc = 'Code Companion - Refactor', mode = 'v' },
+    { mapping_key_prefix .. 'R', '<cmd>CodeCompanion /review<cr>', desc = 'Code Companion - Review', mode = 'v' },
+    { mapping_key_prefix .. 'n', '<cmd>CodeCompanion /naming<cr>', desc = 'Code Companion - Better naming', mode = 'v' },
   },
 }
