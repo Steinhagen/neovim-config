f866877461a8018f7d25be143d22c62c07058a70
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2026-02-13 13:40:31 +0200

    toggleterm: change default shortcut to C-\
    Changed the shortcut to C-\ to leave the default C-t for stack back. This shortcut is really useful and should be preserved.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     require('toggleterm').setup {
-      open_mapping = '<C-t>',
+      open_mapping = [[<C-\>]],
       start_in_insert = true,
       direction = 'float',
       float_opts = {
