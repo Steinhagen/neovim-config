b18237e06ae0e2aed431c3cbeeb64b0b44544b22
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2026-02-25 23:56:20 +0200

    cleanup packages and nvim location
    Make sure that all packages are installed correctly in ordering.

--- init.lua
+++ init.lua
@@ -2,18 +2,6 @@ require 'core.options'
 require 'core.menu'
 require 'core.keymaps'
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
-
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
 -- Load plugins using the default 'pack' package manager
 require 'plugins.lsp'
 require 'plugins.none-ls' -- depends on lsp
@@ -25,6 +13,18 @@ require 'plugins.treesitter'
 require 'plugins.nvim-regexplainer' -- depends on treesitter
 require 'plugins.render-markdown' -- depends on treesitter
 require 'plugins.vim-tmux-navigator'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
 
 -- Load plugins using the 'lazy' package manager
 require('lazy').setup({
