7bb0e8b060e52f6ca9a2a2cc7663a5b80fd30109
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2026-04-18 23:08:11 +0300

    toggleterm: defer setup until first keypress
    Replace the eager require('toggleterm').setup call with a lazy keymap on <C-\>. The plugin is only configured and toggled the first time the keybind is pressed, removing it from the startup path entirely.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,11 +2,17 @@ vim.pack.add({
   'https://github.com/akinsho/toggleterm.nvim',
 }, { confirm = false })
 
-require('toggleterm').setup {
-  open_mapping = [[<C-\>]],
-  start_in_insert = true,
-  direction = 'float',
-  float_opts = {
-    border = 'curved',
-  },
-}
+-- Defer setup until the toggle keybind is first pressed
+local toggled = false
+vim.keymap.set({ 'n', 't' }, [[<C-\>]], function()
+  if not toggled then
+    require('toggleterm').setup {
+      open_mapping = [[<C-\>]],
+      start_in_insert = true,
+      direction = 'float',
+      float_opts = { border = 'curved' },
+    }
+    toggled = true
+  end
+  vim.cmd 'ToggleTerm'
+end, { desc = 'Toggle terminal' })
