adb26451ff68b71220bee10cc7efd679aa1a0d28
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2026-04-18 23:23:10 +0300

    nvim-ufo: fix fillchars icons lost during conversion
    The Nerd Font icons for foldopen (U+F47C) and foldclose (U+F460) were stripped during the file write. Use explicit UTF-8 byte escape sequences to preserve them.

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -7,7 +7,7 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.fillchars = 'eob: ,fold: ,foldopen:' .. '\xef\x91\xbc' .. ',foldsep: ,foldclose:' .. '\xef\x91\xa0'
 
 vim.api.nvim_create_autocmd('BufReadPost', {
   once = true,
