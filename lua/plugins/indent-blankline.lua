d919ce2137a4a00f0ce0cf7ca44407d735c178b5
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2026-04-18 23:14:23 +0300

    indent-blankline: convert from lazy to pack
    Migrate indent-blankline.nvim from lazy.nvim to vim.pack. Setup is deferred via a BufReadPre autocmd with once=true so indentation guides only initialize when a buffer is first read.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,25 +1,30 @@
-return {
-  'lukas-reineke/indent-blankline.nvim',
-  main = 'ibl',
-  opts = {
-    indent = {
-      char = '▏',
-    },
-    scope = {
-      show_start = false,
-      show_end = false,
-      show_exact_scope = false,
-    },
-    exclude = {
-      filetypes = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
+vim.pack.add({
+  'https://github.com/lukas-reineke/indent-blankline.nvim',
+}, { confirm = false })
+
+vim.api.nvim_create_autocmd('BufReadPre', {
+  once = true,
+  callback = function()
+    require('ibl').setup {
+      indent = {
+        char = '▏',
       },
-    },
-  },
-}
+      scope = {
+        show_start = false,
+        show_end = false,
+        show_exact_scope = false,
+      },
+      exclude = {
+        filetypes = {
+          'help',
+          'startify',
+          'dashboard',
+          'packer',
+          'neogitstatus',
+          'NvimTree',
+          'Trouble',
+        },
+      },
+    }
+  end,
+})
