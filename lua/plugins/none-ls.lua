4200043e010f3d46774e1b7b63756f541d0cbee0
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2026-04-18 23:09:21 +0300

    none-ls: defer setup until first BufReadPost
    Wrap the entire none-ls and mason-null-ls setup in a BufReadPost autocmd with once=true. This avoids eagerly loading plenary, null-ls, and mason-null-ls at startup since linters and formatters are not needed until a buffer is actually read.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -29,58 +29,64 @@ end
 -- Keymap to toggle format on save
 vim.keymap.set('n', '<leader>f', toggle_autoformat, { noremap = true, silent = true, desc = 'Toggle format on save' })
 
--- Mason Integration (Skip if on NixOS)
-if not utils.is_nixos() then
-  require('mason-null-ls').setup {
-    ensure_installed = {
-      'prettier', -- ts/js formatter
-      'eslint_d', -- ts/js linter
-      'shfmt', -- Shell formatter
-      'checkmake', -- linter for Makefiles
-    },
-    automatic_installation = true,
-  }
-end
-
--- None-LS Setup
-local null_ls = require 'null-ls'
-local util = require 'lspconfig.util'
-local formatting = null_ls.builtins.formatting
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
-null_ls.setup {
-  sources = {
-    -- Diagnostics: checkmake
-    null_ls.builtins.diagnostics.checkmake.with {
-      extra_args = function(params)
-        local root = util.root_pattern '.git'(params.bufname) or vim.loop.cwd()
-        local config_file = root .. '/.checkmake.ini'
-        if vim.fn.filereadable(config_file) == 1 then
-          return { '--config', config_file }
-        end
-        return {}
-      end,
-    },
-    -- Formatters
-    formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
-    formatting.stylua,
-    formatting.shfmt.with { args = { '-i', '4' } },
-    formatting.terraform_fmt,
-    require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-    require 'none-ls.formatting.ruff_format',
-  },
-  on_attach = function(client, bufnr)
-    if client:supports_method 'textDocument/formatting' then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          if format_on_save_enabled then
-            vim.lsp.buf.format { async = false }
-          end
-        end,
-      })
+-- Defer none-ls and mason-null-ls setup until a buffer is first read
+vim.api.nvim_create_autocmd('BufReadPost', {
+  once = true,
+  callback = function()
+    -- Mason Integration (Skip if on NixOS)
+    if not utils.is_nixos() then
+      require('mason-null-ls').setup {
+        ensure_installed = {
+          'prettier', -- ts/js formatter
+          'eslint_d', -- ts/js linter
+          'shfmt', -- Shell formatter
+          'checkmake', -- linter for Makefiles
+        },
+        automatic_installation = true,
+      }
     end
+
+    -- None-LS Setup
+    local null_ls = require 'null-ls'
+    local util = require 'lspconfig.util'
+    local formatting = null_ls.builtins.formatting
+    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+    null_ls.setup {
+      sources = {
+        -- Diagnostics: checkmake
+        null_ls.builtins.diagnostics.checkmake.with {
+          extra_args = function(params)
+            local root = util.root_pattern '.git'(params.bufname) or vim.loop.cwd()
+            local config_file = root .. '/.checkmake.ini'
+            if vim.fn.filereadable(config_file) == 1 then
+              return { '--config', config_file }
+            end
+            return {}
+          end,
+        },
+        -- Formatters
+        formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
+        formatting.stylua,
+        formatting.shfmt.with { args = { '-i', '4' } },
+        formatting.terraform_fmt,
+        require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
+        require 'none-ls.formatting.ruff_format',
+      },
+      on_attach = function(client, bufnr)
+        if client:supports_method 'textDocument/formatting' then
+          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              if format_on_save_enabled then
+                vim.lsp.buf.format { async = false }
+              end
+            end,
+          })
+        end
+      end,
+    }
   end,
-}
+})
